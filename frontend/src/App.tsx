198a5e37624f2ead8cd64b9a0cd456192a2f6e28
E-dmp <ridemdem1510@gmail.com>
2022-12-01 00:50:43 +0900

    feature/get-tweet-data #3

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,17 +1,19 @@
 import axios from 'axios'
 import { useState } from 'react'
+import { nanoid } from 'nanoid'
 
-type Sample = {
-  Hello: string
+type Tweet = {
+  TweetData:any
+  text: string
 }
 
 function App () {
-  const [data, setData] = useState<Sample>()
+  const [data, setTweetData] = useState<Tweet>()
   const url = 'http://127.0.0.1:8000'
 
   const getTweetData = () => {
     axios.get(url).then((res) => {
-      setData(res.data)
+      setTweetData(res.data)
       console.log(res.data)
     })
   }
@@ -20,10 +22,14 @@ function App () {
       <div>感情分析アプリ</div>
       <div>
         {data ? (
-          <div>{data.Hello}</div>
+          <div>{data.TweetData.map((twdata: string[]) => (
+            <li key={nanoid()}>{twdata}</li>
+          ))}</div>
         ) : (
           <button onClick={getTweetData}>データを取得</button>
         )}
+
+
       </div>
     </div>
   )
